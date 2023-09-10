201ac053d5eedb87cfe631b0e239c57cd0df552c
BuildTools <unconfigured@null.spigotmc.org>
2023-09-10 18:37:36 -0300

    Initial commit

---
+++ src/main/java/com/rsffa/RSF.java
@@ -0,0 +1,17 @@
+package com.rsffa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RSF extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
